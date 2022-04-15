5137aad9f0c18fd042182093dfe8d3c7c0b6d4c1
StepaniukJulia <juliastepaniuk2510@gmail.com>
2022-04-15 19:35:48 +0300

    Lesson 1 js task 1 was done.Code cleanup.

---
+++ js/lesson1/task1.js
@@ -0,0 +1,95 @@
+let a = 'hello' ;
+console.log(a) ;
+
+let b = 'owu' ;
+console.log(b) ;
+
+let c = 'com' ;
+console.log(c) ;
+
+let d = 'ua' ;
+console.log(d) ;
+
+let e = 1 ;
+console.log(e) ;
+
+let f = 10 ;
+console.log(f) ;
+
+let g = -999 ;
+console.log(g) ;
+
+let h = 123 ;
+console.log(h) ;
+
+let i = 3.14 ;
+console.log(i) ;
+
+let j = 2.7 ;
+console.log(j) ;
+
+let k = 16 ;
+console.log(k) ;
+
+let bool1 = (5<6) ;
+console.log(bool1) ;
+
+let bool2 = (5>6) ;
+console.log(bool2) ;
+
+
+document.write([a, b, c, d, e, f, g, h, i, j, k, bool1, bool2]) ;
+
+alert([a, b, c, d, e, f, g, h, i, j, k, bool1, bool2]) ;
+
+
+
+let book1 = {
+    title: 'The Godfather',
+    pages: 300,
+    genre: 'Crime novel',
+} ;
+
+console.log(book1) ;
+
+let book2 = {
+    title: 'Physical intelligence',
+    pages: 450,
+    genre: 'Psychology',
+    authors: ['Claire Dale', 'Patricia Peyton']
+} ;
+
+let firstName = 'Yuliia' ;
+let midlName = 'Volodymyrivna' ;
+let lastName = 'Stepaniuk' ;
+let person = (firstName + ' ' + midlName + ' ' + lastName) ;
+
+let fn = prompt( 'enter your first name') ;
+let mn = prompt( 'enter your midl name') ;
+let age = prompt( 'enter your age') ;
+
+let person1 = `${fn} ${mn} ${age}` ;
+console.log(person1) ;
+
+let a1 = 100 ;
+console.log(typeof a1) ;
+
+let b1 = '100' ;
+console.log(typeof b1) ;
+let c1 = 5<6 ;
+console.log(typeof c1) ;
+
+let books = [
+    {title: 'Ark de Triomphe', pages: 400, genre: 'novel', author: 'E.M.Remark' },
+    {title: 'The Metamorphosis', pages: 200, genre: 'short story', author: 'F.Kafka' },
+    {title: '1984', pages: 300, genre: 'novel', author: 'G.Orwell' },
+    {title: 'The Great Gatsby', pages: 450, genre: 'novel', author: 'F.S.Fitzgerald' },
+] ;
+console.log(books[0]) ;
+console.log(books[1]) ;
+console.log(books[2]) ;
+console.log(books[3]) ;
+
+
+
+

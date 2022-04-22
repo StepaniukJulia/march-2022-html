6a22348e23d3a277ecf1491fb3d0a979650b4fc5
StepaniukJulia <juliastepaniuk2510@gmail.com>
2022-04-22 15:36:35 +0300

    Lesson 2 js and lesson 3 js were done.

---
+++ js/lesson2/task1.js
@@ -0,0 +1,60 @@
+// let x = +prompt('enter x')
+//
+// if (x === 0){
+//     console.log('virno');
+// }else {
+//     console.log('ne virno');
+// }
+
+// let time = +prompt('enter from 0 to 59');
+// if (time <=14){
+//     console.log('part1');
+// } else if (time >=15 && time <=29){
+//     console.log('part2');
+// } else if (time >=30 && time <=44){
+//     console.log('part3');
+// } else if (time >=45 && time <=59){
+//     console.log('part4')
+// }
+// let day = +prompt('enter from 1 to 31');
+// if (day >=1 && day <=10 ){
+//     console.log('decade1');
+// } else if (day >=11 && day <=20 ){
+//     console.log('decade2');
+// } else if (day >=21 && day <=31){
+//     console.log('decade3');
+// }
+// let timeTable = +prompt('enter from 1 to 7');
+// switch (timeTable){
+//     case 1 :
+//          console.log('Monday');
+//          break;
+//     case 2 :
+//         console.log('Tuesday');
+//         break;
+//     case 3 :
+//         console.log('Wednesday');
+//         break;
+//     case 4 :
+//         console.log('Thursday');
+//         break;
+//     case 5 :
+//         console.log('Friday');
+//         break;
+//     case 6 :
+//         console.log('Saturday');
+//         break;
+//     case 7 :
+//         console.log('Sunday');
+//         break;
+// }
+
+// let a = 0;
+// let b = 0;
+// if(a > b){
+//     console.log(a);
+// }else if(b > a){
+//     console.log(b);
+// }else if(a === b){
+//     console.log('equal');
+// }

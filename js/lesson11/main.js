fb238703c340f671d8e6583000d44d2e4cc90183
StepaniukJulia <juliastepaniuk2510@gmail.com>
2022-06-04 11:55:13 +0300

    Lesson 11 was done.

---
+++ js/lesson11/main.js
@@ -0,0 +1,35 @@
+// -створити форму з інпутами для name та age.
+//     При відправці форми записати об'єкт в localstorage
+// const {name, age} = document.forms.form1;
+// const button = document.getElementById('button');
+// const key = 'key'
+//
+// const save = (name, age) =>{
+//     const object = {
+//         name,
+//         age
+//     }
+//     localStorage.setItem(key,JSON.stringify(object));
+// }
+// button.onclick = () => {
+//     save(name.value, age.value);
+// }
+// -створити форму з інпутами для model,type та volume автівки.
+//     при відпарвці форми об'єкти зберігаються в масиві в локальному сховищі.
+
+
+// const  {model, type, volume} = document.forms.form2;
+// const button = document.getElementById('button');
+// const arr = [];
+//
+// const save = (model, type, volume) =>{
+//     arr.push({model, type, volume})
+//     localStorage.setItem('carKey',JSON.stringify(arr));
+//
+// }
+// button.onclick = () =>{
+//     save(model.value, type.value, volume.value );
+//
+// }
+//
+
